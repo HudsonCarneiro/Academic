2eca3a7a08725c3392939ac0f849d20b20212dfc
Hudson <161895030+HudsonCarneiro@users.noreply.github.com>
2025-03-07 20:24:39 -0300

    prog IV - menu

---
+++ Academic-main/C#-Principles/ATIVIDADE_01/ProgramacaoIV.Exercicios/Janelas/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace ProgramacaoIV.Exercicios.Janelas
+{
+    
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnActivity01_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnActivity02_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnActivity03_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnActivity04_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
