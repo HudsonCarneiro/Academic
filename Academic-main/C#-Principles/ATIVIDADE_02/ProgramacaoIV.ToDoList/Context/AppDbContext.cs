e615444596d45a3b25d9694d830d8b605391a0e4
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2025-03-26 01:47:18 -0300

    conclued start

---
+++ Academic-main/C#-Principles/ATIVIDADE_02/ProgramacaoIV.ToDoList/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgramacaoIV.ToDoList.Model;
+
+namespace ProgramacaoIV.ToDoList.Context;
+
+public sealed class TodoContext : DbContext
+{
+    public DbSet<Todo> Todos { get; set; }
+
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options) 
+    {
+        if (Database.GetPendingMigrations().Any())
+            Database.Migrate();
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        #region todo
+
+        modelBuilder.Entity<Todo>().HasKey(t => t.Id);
+        modelBuilder.Entity<Todo>().Property(t => t.Title).IsRequired();
+        modelBuilder.Entity<Todo>().Property(t => t.IsDone).IsRequired();
+
+        #endregion todo
+    }
+}
