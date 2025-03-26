bbb90a9e3e993439f545d4f2b241dd25ce34bd87
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2025-03-26 01:27:41 -0300

    delete minimal api

--- Academic-main/C#-Principles/ATIVIDADE_02/minimal-api-main/ProgramacaoIV.Atv02/Model/Todo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProgramacaoIV.ToDoList.Model;
-
-public sealed class Todo
-{
-    public Guid Id { get; private set; } = Guid.NewGuid();
-    public string Title { get; set; } = string.Empty;
-    public bool IsDone { get; set; } = false;
-}
