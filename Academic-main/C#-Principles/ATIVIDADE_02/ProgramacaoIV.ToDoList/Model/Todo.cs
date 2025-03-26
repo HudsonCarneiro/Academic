f49b6c637c044fbdfd2dbe311f84502f0abb1ac4
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2025-03-26 01:28:40 -0300

    minimal api

---
+++ Academic-main/C#-Principles/ATIVIDADE_02/ProgramacaoIV.ToDoList/Model/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgramacaoIV.ToDoList.Model;
+
+public sealed class Todo
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public bool IsDone { get; set; } = false;
+}
