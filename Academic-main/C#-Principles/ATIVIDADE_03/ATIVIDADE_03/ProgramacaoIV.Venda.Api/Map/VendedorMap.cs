50463a773db50c8408c2e3701287818c5b75dbc7
Hudson <161895030+HudsonCarneiro@users.noreply.github.com>
2025-03-28 21:25:19 -0300

    atv03

---
+++ Academic-main/C#-Principles/ATIVIDADE_03/ATIVIDADE_03/ProgramacaoIV.Venda.Api/Map/VendedorMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgramacaoIV.Venda.Api.Entidades;
+
+namespace ProgramacaoIV.Venda.Api.Map;
+
+public sealed class VendedorMap : AbstractEntidadeMap<Vendedor>
+{
+    public override void Configure(EntityTypeBuilder<Vendedor> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("VENDEDOR");
+
+        builder.Property(x => x.Nome).HasColumnName("NM_VENDEDOR").HasMaxLength(100).IsRequired();
+        builder.Property(x => x.Email).HasColumnName("EM_VENDEDOR").HasMaxLength(200).IsRequired();
+
+    }
+}
